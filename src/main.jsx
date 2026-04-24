6376b649bfb03607cf03c91c87cbe02df91e5737
shompaaa <shompaakter334@gmail.com>
2026-04-24 13:11:33 +0600

    create page, navigate route and customize theme

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Root from './layouts/Root.jsx'
+import Home from './components/Home/Home.jsx'
+import AllProducts from './components/AllProducts/AllProducts.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    Component: Root,
+    children:[
+      {
+        index: true,
+        Component: Home
+      },
+      {
+        path: '/allProducts',
+        Component: AllProducts
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
