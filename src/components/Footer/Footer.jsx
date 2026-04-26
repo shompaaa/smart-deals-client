96fd77d73c0858c983a898908d333706465ca701
shompaaa <shompaakter334@gmail.com>
2026-04-26 12:05:29 +0600

    footer page done

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link } from "react-router";
+
+const Footer = () => {
+  return (
+    <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10 px-20">
+      <aside className="max-w-xs">
+        <Link to="/" className="text-xl">
+          Smart<span className="text-primary">Deals</span>
+        </Link>
+        <p>
+          Your trusted marketplace for authentic local products. Discover the
+          best deals from across Bangladesh.
+        </p>
+      </aside>
+      <nav>
+        <h6 className="footer-title">Quick Links</h6>
+        <Link to="/allProducts" className="link link-hover">
+          All Products
+        </Link>
+        <Link to="/dashboard" className="link link-hover">
+          Dashboard
+        </Link>
+        <Link to="/login" className="link link-hover">
+          Login
+        </Link>
+        <Link to="/register" className="link link-hover">
+          Register
+        </Link>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Categories</h6>
+        <Link to="/categories" className="link link-hover">
+          Electronics
+        </Link>
+        <Link to="/fashion" className="link link-hover">
+          Fashion
+        </Link>
+        <Link to="/homeAndLiving" className="link link-hover">
+          Home & Living
+        </Link>
+        <Link to="/groceries" className="link link-hover">
+          Groceries
+        </Link>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Contact & Support</h6>
+        <p>support@smart-deals.com</p>
+        <p>+88012345678</p>
+        <p>123 Commerce Street, Dhaka, Bangladesh</p>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Social Links</h6>
+      </nav>
+    </footer>
+  );
+};
+
+export default Footer;
